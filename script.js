8b187bfd0936cd95819217d76f3a189a37fc53a2
smukx <120155672+smukx@users.noreply.github.com>
2023-04-12 06:04:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//JavaScript code
+
+window.onload = function() {
+const logo = document.querySelector('.logo img');
+
+logo.addEventListener('click', function() {
+  alert('This page is Redirecting to a Main one.. Click ok to proceed!');
+  location.href=("https://smukx.pagekite.me")
+  alert('Welcome to Smukx Blog');
+
+});
+}
+
+
+
+
+
+
+
+  
