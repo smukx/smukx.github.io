e45b5021f861588567360c2177b45306222eb6ed
smukx <120155672+smukx@users.noreply.github.com>
2023-04-12 06:08:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const logo = document.querySelector('.logo img');
 
 logo.addEventListener('click', function() {
   alert('This page is Redirecting to a Main one.. Click ok to proceed!');
-  location.href=("https://smukx.pagekite.me")
+  location.href=("https://smukx.github.io")
   alert('Welcome to Smukx Blog');
 
 });
