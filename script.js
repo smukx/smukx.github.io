d2fff7183b97af59a69da35b044f51c5b5ffbc97
smukx <120155672+smukx@users.noreply.github.com>
2024-09-04 02:26:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ window.onload = function() {
 const logo = document.querySelector('.logo img');
 
 logo.addEventListener('click', function() {
-  alert('This page is Redirecting to a Main one.. Click ok to proceed!');
-  location.href=("https://smukx.github.io")
-  alert('Welcome to Smukx Blog');
+  // alert('This page is Redirecting to a Main one.. Click ok to proceed!');
+  location.href=("./home.html")
+  // alert('Welcome to Smukx Blog');
 
 });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.querySelector('.nav-toggle');
+  const navList = document.querySelector('.nav-list');
+
+  navToggle.addEventListener('click', function () {
+      navList.classList.toggle('nav-active');
+  });
+});
+
 
 
 
